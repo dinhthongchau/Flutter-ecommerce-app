b713ca6a2a8b942afa4745ef177cf91df95be8bd
Chau Dinh Thong <dinhthongchau@gmail.com>
2025-03-26 19:12:19 +0700

    feat: add logger package for Logs

--- lib/repositories/log_implements.dart
+++ lib/repositories/log_implements.dart
@@ -1,18 +1,14 @@
+import 'package:logger/logger.dart';
+
 import 'log.dart';
 
 class LogImplement implements Log {
-  @override
-  void d(String tag, String content) {
-    print("$tag $content");
-  }
+  final Logger _logger = Logger();
 
   @override
-  void e(String tag, String content) {
-    print("$tag $content");
-  }
-
+  void i(String tag, String content) => _logger.i('$tag: $content');
   @override
-  void i(String tag, String content) {
-    print("$tag $content");
-  }
+  void d(String tag, String content) => _logger.d('$tag: $content');
+  @override
+  void e(String tag, String content) => _logger.e('$tag: $content');
 }
